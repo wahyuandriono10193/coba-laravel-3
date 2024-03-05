21fe4c3cc65326955679b21ea30fc41b38770e40
wahyu andriono <wahyuandriono10193@gmail.com>
2024-03-05 12:11:40 +0700

    update & delete post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,8 +6,11 @@
             <h1 class="mb-5">{{ $post->title }}</h1>
             <a href="/dashboard/posts" class="btn btn-success"> <span data-feather="arrow-left"></span>Back to all my post</a>
             <a href="" class="btn btn-success"> <span data-feather="edit"></span>edit</a>
-            <a href="" class="btn btn-danger"> <span data-feather="x-circle"></span>delete</a>
-            
+            <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger" onclick="return confirm('are you sure?')"><span data-feather="x-circle"></span>Delete</button>
+              </form> 
             <img src="https://source.unsplash.com/200x75?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
             <article class="my-3 fs-5">
             {!! $post->body !!}
